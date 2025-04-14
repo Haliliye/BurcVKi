eeaf210d5fd29b7dde27b8a2cfb4a2b0db3cc93b
Haliliye <161294480+Haliliye@users.noreply.github.com>
2025-04-14 16:45:39 +0300

    Add files via upload

---
+++ BurcVKİ/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace BurcVKIProjesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAd = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.lblSoyad = new System.Windows.Forms.Label();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.lblDogum = new System.Windows.Forms.Label();
+            this.dtpDogumTarihi = new System.Windows.Forms.DateTimePicker();
+            this.lblBoy = new System.Windows.Forms.Label();
+            this.txtBoy = new System.Windows.Forms.TextBox();
+            this.lblKilo = new System.Windows.Forms.Label();
+            this.txtKilo = new System.Windows.Forms.TextBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.Location = new System.Drawing.Point(68, 52);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(20, 13);
+            this.lblAd.TabIndex = 0;
+            this.lblAd.Text = "Ad";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(94, 45);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(100, 20);
+            this.txtAd.TabIndex = 1;
+            // 
+            // lblSoyad
+            // 
+            this.lblSoyad.AutoSize = true;
+            this.lblSoyad.Location = new System.Drawing.Point(51, 87);
+            this.lblSoyad.Name = "lblSoyad";
+            this.lblSoyad.Size = new System.Drawing.Size(37, 13);
+            this.lblSoyad.TabIndex = 2;
+            this.lblSoyad.Text = "Soyad";
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(94, 80);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtSoyad.TabIndex = 3;
+            // 
+            // lblDogum
+            // 
+            this.lblDogum.AutoSize = true;
+            this.lblDogum.Location = new System.Drawing.Point(18, 118);
+            this.lblDogum.Name = "lblDogum";
+            this.lblDogum.Size = new System.Drawing.Size(70, 13);
+            this.lblDogum.TabIndex = 4;
+            this.lblDogum.Text = "Doğum Tarihi";
+            // 
+            // dtpDogumTarihi
+            // 
+            this.dtpDogumTarihi.Location = new System.Drawing.Point(94, 112);
+            this.dtpDogumTarihi.Name = "dtpDogumTarihi";
+            this.dtpDogumTarihi.Size = new System.Drawing.Size(200, 20);
+            this.dtpDogumTarihi.TabIndex = 5;
+            // 
+            // lblBoy
+            // 
+            this.lblBoy.AutoSize = true;
+            this.lblBoy.Location = new System.Drawing.Point(64, 156);
+            this.lblBoy.Name = "lblBoy";
+            this.lblBoy.Size = new System.Drawing.Size(25, 13);
+            this.lblBoy.TabIndex = 6;
+            this.lblBoy.Text = "Boy";
+            // 
+            // txtBoy
+            // 
+            this.txtBoy.Location = new System.Drawing.Point(94, 149);
+            this.txtBoy.Name = "txtBoy";
+            this.txtBoy.Size = new System.Drawing.Size(100, 20);
+            this.txtBoy.TabIndex = 7;
+            // 
+            // lblKilo
+            // 
+            this.lblKilo.AutoSize = true;
+            this.lblKilo.Location = new System.Drawing.Point(64, 188);
+            this.lblKilo.Name = "lblKilo";
+            this.lblKilo.Size = new System.Drawing.Size(24, 13);
+            this.lblKilo.TabIndex = 8;
+            this.lblKilo.Text = "Kilo";
+            // 
+            // txtKilo
+            // 
+            this.txtKilo.Location = new System.Drawing.Point(94, 185);
+            this.txtKilo.Name = "txtKilo";
+            this.txtKilo.Size = new System.Drawing.Size(100, 20);
+            this.txtKilo.TabIndex = 9;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(94, 224);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(100, 35);
+            this.btnKaydet.TabIndex = 10;
+            this.btnKaydet.Text = "Hesapla ve Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.Location = new System.Drawing.Point(420, 45);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(250, 100);
+            this.lblSonuc.TabIndex = 11;
+            this.lblSonuc.Text = "Hesap Sonucu";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(451, 149);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(194, 237);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.txtKilo);
+            this.Controls.Add(this.lblKilo);
+            this.Controls.Add(this.txtBoy);
+            this.Controls.Add(this.lblBoy);
+            this.Controls.Add(this.dtpDogumTarihi);
+            this.Controls.Add(this.lblDogum);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.lblSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.lblAd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAd;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label lblSoyad;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.Label lblDogum;
+        private System.Windows.Forms.DateTimePicker dtpDogumTarihi;
+        private System.Windows.Forms.Label lblBoy;
+        private System.Windows.Forms.TextBox txtBoy;
+        private System.Windows.Forms.Label lblKilo;
+        private System.Windows.Forms.TextBox txtKilo;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ BurcVKİ/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Globalization;
+
+namespace BurcVKIProjesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string GetBurc(int gun, int ay)
+        {
+            if ((ay == 1 && gun >= 20) || (ay == 2 && gun <= 18)) return "Kova";
+            if ((ay == 2 && gun >= 19) || (ay == 3 && gun <= 20)) return "Balık";
+            if ((ay == 3 && gun >= 21) || (ay == 4 && gun <= 19)) return "Koç";
+            if ((ay == 4 && gun >= 20) || (ay == 5 && gun <= 20)) return "Boğa";
+            if ((ay == 5 && gun >= 21) || (ay == 6 && gun <= 21)) return "İkizler";
+            if ((ay == 6 && gun >= 22) || (ay == 7 && gun <= 22)) return "Yengeç";
+            if ((ay == 7 && gun >= 23) || (ay == 8 && gun <= 22)) return "Aslan";
+            if ((ay == 8 && gun >= 23) || (ay == 9 && gun <= 22)) return "Başak";
+            if ((ay == 9 && gun >= 23) || (ay == 10 && gun <= 22)) return "Terazi";
+            if ((ay == 10 && gun >= 23) || (ay == 11 && gun <= 21)) return "Akrep";
+            if ((ay == 11 && gun >= 22) || (ay == 12 && gun <= 21)) return "Yay";
+            if ((ay == 12 && gun >= 22) || (ay == 1 && gun <= 19)) return "Oğlak";
+            return "Bilinmiyor";
+        }
+
+        string GetBurcYorum(string burc)
+        {
+            switch (burc)
+            {
+                case "Koç": return "Cesur, lider ruhludur.";
+                case "Boğa": return "Sabırlı ve güvenilirdir.";
+                case "İkizler": return "Zeki ve iletişimde kuvvetlidir.";
+                case "Yengeç": return "Duygusal, sadık ve sezgiseldir.";
+                case "Aslan": return "Kendine güvenen ve cömerttir.";
+                case "Başak": return "Titiz ve detaycıdır.";
+                case "Terazi": return "Adil ve dengelidir.";
+                case "Akrep": return "Tutkulu ve gizemlidir.";
+                case "Yay": return "Macera sever ve enerjiktir.";
+                case "Oğlak": return "Disiplinli ve kararlıdır.";
+                case "Kova": return "Bağımsız ve yenilikçidir.";
+                case "Balık": return "Hayalperest ve sezgiseldir.";
+                default: return "Burç yorumu bulunamadı.";
+            }
+        }
+
+        float HesaplaVKI(float boy, float kilo)
+        {
+            return kilo / (boy * boy);
+        }
+
+        string VKIYorum(float vki)
+        {
+            if (vki < 18.5) return "Zayıf";
+            else if (vki < 25) return "Normal";
+            else if (vki < 30) return "Fazla kilolu";
+            else return "Obez";
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            string ad = txtAd.Text;
+            string soyad = txtSoyad.Text;
+            DateTime dogum = dtpDogumTarihi.Value;
+            float boy = float.Parse(txtBoy.Text, CultureInfo.InvariantCulture);
+            float kilo = float.Parse(txtKilo.Text, CultureInfo.InvariantCulture);
+
+            int gun = dogum.Day;
+            string ayIsim = dogum.ToString("MMMM", new CultureInfo("tr-TR"));
+            int aySayi = dogum.Month;
+            int yil = dogum.Year;
+
+            string burc = GetBurc(gun, aySayi);
+            string yorum = GetBurcYorum(burc);
+
+            float vki = HesaplaVKI(boy, kilo);
+            string vkiYorum = VKIYorum(vki);
+
+            string burcResmi = $"burclar\\{burc.ToLower().Replace("ç", "c").Replace("ğ", "g").Replace("ı", "i").Replace("ö", "o").Replace("ş", "s").Replace("ü", "u")}.jpg";
+
+            if (System.IO.File.Exists(burcResmi))
+                pictureBox1.Image = Image.FromFile(burcResmi);
+            else
+                pictureBox1.Image = null;
+
+            lblSonuc.Text = $"Burç: {burc}\nYorum: {yorum}\nVKİ: {vki:F1} - {vkiYorum}";
+
+            string connectionString = "Server=localhost;Database=BurcVKI;Trusted_Connection=True;";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "INSERT INTO Kisiler (Ad, Soyad, Gun, Ay, Yil, Burc, Yorum, BurcResmi, VKI, VKIYorum) " +
+                             "VALUES (@ad, @soyad, @gun, @ay, @yil, @burc, @yorum, @resim, @vki, @vkiyorum)";
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@ad", ad);
+                    cmd.Parameters.AddWithValue("@soyad", soyad);
+                    cmd.Parameters.AddWithValue("@gun", gun);
+                    cmd.Parameters.AddWithValue("@ay", ayIsim);
+                    cmd.Parameters.AddWithValue("@yil", yil);
+                    cmd.Parameters.AddWithValue("@burc", burc);
+                    cmd.Parameters.AddWithValue("@yorum", yorum);
+                    cmd.Parameters.AddWithValue("@resim", burcResmi);
+                    cmd.Parameters.AddWithValue("@vki", vki);
+                    cmd.Parameters.AddWithValue("@vkiyorum", vkiYorum);
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Bilgiler başarıyla kaydedildi!");
+                }
+            }
+        }
+    }
+}
